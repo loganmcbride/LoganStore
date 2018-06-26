90ade4a79c425178a96086675e7edeab29e84bd5
Logan McBride <loganmcbride@Logans-MacBook-Pro.local>
2018-06-26 10:29:43 -0400

    creating a repo for first DY sample store

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '/')));
+
+app.get('/', function(req, res){
+    res.send('Hello World!');
+});
+
+app.listen(3000, function(){
+    console.log('listening on Port 3000!')
+})
